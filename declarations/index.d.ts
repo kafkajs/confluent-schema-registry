9b231ec434349aaae6357886fe2a1ff77da7b0d5
Erik Engervall <erik.engervall@klarna.com>
2019-08-21 19:58:03 +0200

    library module declaration

---
+++ declarations/index.d.ts
@@ -0,0 +1,52 @@
+declare module 'confluent-schema-registry' {
+  export interface SchemaRegistryCache {
+    getLatestRegistryId: (subject: string) => any
+    setLatestRegistryId: (subject: string, id: number) => void
+    getSchema: (registryId: number) => Schema
+    setSchema: (registryId: number, schema: Schema) => void
+  }
+
+  export interface Schema {
+    name: string
+    namespace: string
+  }
+
+  export interface RegisteredSchema {
+    id: number
+  }
+
+  export type Compatibility = 'NONE' | 'FULL' | 'BACKWARD' | 'FORWARD'
+
+  export interface SchemaRegistryRegisterOptions {
+    compatibility: Compatibility
+    separator: string
+  }
+
+  export interface SchemaRegistry {
+    cache: SchemaRegistryCache
+    register(schema: Schema, options?: SchemaRegistryRegisterOptions): Promise<RegisteredSchema>
+    getSchema(registryId: number): Promise<Schema>
+    encode(registryId: number, jsonPayload: any): Promise<Buffer>
+    decode(buffer: Buffer): Promise<any>
+    getRegistryId(subject: string, version: number): number
+  }
+
+  export interface SchemaRegistryConstructorRetryOptions {
+    maxRetryTimeInSecs: number
+    initialRetryTimeInSecs: number
+    factor: number
+    multiplier: number
+    retries: number
+  }
+
+  export interface SchemaRegistryConstructorArgs {
+    host: string
+    retry?: SchemaRegistryConstructorRetryOptions
+  }
+
+  export type SchemaRegistryConstructor = new (
+    args: SchemaRegistryConstructorArgs,
+  ) => SchemaRegistry
+
+  export const SchemaRegistry: SchemaRegistryConstructor
+}
