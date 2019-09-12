ab77722d4a00437ef14616c989e0d0142b9844cc
tulios <ornelas.tulio@gmail.com>
2019-09-12 17:54:16 +0200

    Update eslintrc rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,7 @@
 module.exports = {
   root: true,
   extends: [
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'prettier',
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   parser: '@typescript-eslint/parser',
@@ -18,17 +16,26 @@ module.exports = {
   env: {
     node: true,
   },
-  rules: {
-    'no-console': 'error',
-    'no-only-tests/no-only-tests': [
-      'error',
-      { block: ['test', 'it', 'assert'], focus: ['only', 'focus'] },
-    ],
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      extends: [
+        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+        'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+      ],
+      rules: {
+        'no-console': 'error',
+        'no-only-tests/no-only-tests': [
+          'error',
+          { block: ['test', 'it', 'assert'], focus: ['only', 'focus'] },
+        ],
 
-    // Typescript
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-use-before-define': 'error',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/ban-ts-ignore': 'warn',
-  },
+        // Typescript
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-use-before-define': 'error',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/ban-ts-ignore': 'warn',
+      },
+    },
+  ],
 }
