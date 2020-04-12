5e15c78e9e6e6056035ee9c20d952e07c33f5a3a
Tommy Brunn <tommy.brunn@gmail.com>
2020-04-12 14:38:33 +0200

    Ignore nested node_modules in test watch

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
     },
   },
   testPathIgnorePatterns: ['/node_modules/'],
+  watchPathIgnorePatterns: ['/node_modules/'],
   roots: ['.'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   projects: ['<rootDir>/src'],
