f78b2e359bd5c00c03e520f991808e2a0d27c5e6
Nishant Arora <nishant.arora@appdirect.com>
2021-02-08 10:52:43 -0700

    Adding caching for schemaId bey name and version

--- src/cache.ts
+++ src/cache.ts
@@ -1,16 +1,29 @@
 import avro, { ForSchemaOptions } from 'avsc'
 
-import { Schema } from './@types'
+import { Schema, SchemaRef } from './@types'
 
 export default class Cache {
   registryIdBySubject: { [key: string]: number }
   schemasByRegistryId: { [key: string]: Schema }
+  registryIdBySchemaRef: { [key: string]: number }
   forSchemaOptions?: Partial<ForSchemaOptions>
 
   constructor(forSchemaOptions?: Partial<ForSchemaOptions>) {
     this.registryIdBySubject = {}
     this.schemasByRegistryId = {}
+    this.registryIdBySchemaRef = {}
     this.forSchemaOptions = forSchemaOptions
+  }
+
+  private schemaKeyGen = ({ subject, version }: SchemaRef): string => `${subject}:${version}`
+
+  getRegistryIdBySchemaRef = (schema: SchemaRef): number =>
+    this.registryIdBySchemaRef[this.schemaKeyGen(schema)]
+
+  setRegistryIdBySchemaRef = (schema: SchemaRef, registryId: number) => {
+    this.registryIdBySchemaRef[this.schemaKeyGen(schema)] = registryId
+
+    return this.registryIdBySchemaRef[this.schemaKeyGen(schema)]
   }
 
   getLatestRegistryId = (subject: string): number | undefined => this.registryIdBySubject[subject]
@@ -23,9 +36,34 @@ export default class Cache {
 
   getSchema = (registryId: number): Schema => this.schemasByRegistryId[registryId]
 
-  setSchema = (registryId: number, schema: Schema) => {
+  setSchema = (
+    registryId: number,
+    schema: Schema,
+    logicalTypesExtra: Record<string, Schema> = {},
+  ) => {
     // @ts-ignore TODO: Fix typings for Schema...
-    this.schemasByRegistryId[registryId] = avro.Type.forSchema(schema, this.forSchemaOptions)
+    this.schemasByRegistryId[registryId] = avro.Type.forSchema(schema, {
+      ...this.forSchemaOptions,
+      typeHook:
+        typeof this.forSchemaOptions?.typeHook === 'function'
+          ? this.forSchemaOptions?.typeHook
+          : (attr, opts) => {
+              if (typeof attr == 'string') {
+                if (attr in opts.logicalTypes) {
+                  return opts.logicalTypes[attr]
+                }
+                // if we map this as 'namespace.type'.
+                const qualifiedName = `${opts.namespace}.${attr}`
+                if (qualifiedName in opts.logicalTypes) {
+                  return opts.logicalTypes[qualifiedName]
+                }
+              }
+            },
+      logicalTypes: {
+        ...this.forSchemaOptions?.logicalTypes,
+        ...logicalTypesExtra,
+      },
+    })
 
     return this.schemasByRegistryId[registryId]
   }
