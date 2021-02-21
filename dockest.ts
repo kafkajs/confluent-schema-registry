f4b52faa1f58cd22a0f2b893bda747b126680680
Dan Katz <dskatz22@gmail.com>
2021-02-20 19:21:09 -0500

    upgrading dockest

--- dockest.ts
+++ dockest.ts
@@ -1,51 +1,30 @@
-import { default as Dockest, runners, logLevel } from 'dockest'
-
-const { ZooKeeperRunner, SimpleRunner, KafkaRunner } = runners
-
-const zooKeeperRunner = new ZooKeeperRunner({
-  service: 'zooKeeper',
-  ports: {
-    [ZooKeeperRunner.DEFAULT_PORT]: ZooKeeperRunner.DEFAULT_PORT,
-  },
-})
-
-/**
- * Debug SchemaRegistry with CURL: https://docs.confluent.io/2.0.0/schema-registry/docs/intro.html
- */
-const schemaRegistryRunner = new SimpleRunner({
-  service: 'schemaRegistry',
-  image: 'confluentinc/cp-schema-registry:5.3.0',
-  ports: {
-    '8982': '8081',
-  },
-  environment: {
-    SCHEMA_REGISTRY_KAFKASTORE_CONNECTION_URL: `${zooKeeperRunner.runnerConfig.service}:${ZooKeeperRunner.DEFAULT_PORT}`,
-    SCHEMA_REGISTRY_HOST_NAME: 'localhost',
-  },
-})
-
-const kafkaRunner = new KafkaRunner({
-  service: 'kafka',
-  image: 'confluentinc/cp-kafka:5.2.2',
-  dependsOn: [zooKeeperRunner],
-  ports: {
-    [KafkaRunner.DEFAULT_PORT_PLAINTEXT]: KafkaRunner.DEFAULT_PORT_PLAINTEXT,
-  },
-})
+import { Dockest, sleepWithLog, logLevel } from 'dockest'
+import { DockestService } from 'dockest/dist/@types'
 
 const dockest = new Dockest({
-  runners: [kafkaRunner, schemaRegistryRunner],
-  jest: {
-    lib: require('jest'),
-    verbose: true,
+  composeFile: 'docker-compose.yml',
+  dumpErrors: true,
+  jestLib: require('jest'),
+  jestOpts: {
+    updateSnapshot: true,
   },
-  opts: {
-    logLevel: logLevel.DEBUG,
-    afterSetupSleep: 35,
-    dev: {
-      debug: process.argv[2] === 'debug' || process.argv[2] === 'dev',
-    },
-  },
+  logLevel: logLevel.DEBUG,
 })
 
-dockest.run()
+const dockestServices: DockestService[] = [
+  {
+    serviceName: 'zooKeeper',
+    dependents: [
+      {
+        serviceName: 'kafka',
+        readinessCheck: () => sleepWithLog(10, `Sleeping for Kafka`),
+      },
+    ],
+  },
+  {
+    serviceName: 'schemaRegistry',
+    readinessCheck: () => sleepWithLog(20, `Sleeping for Schema Registry`),
+  },
+]
+
+dockest.run(dockestServices)
