b448feefd7973d9e7aac0cd3aeac3994363f3b45
Ringo Yip <ringo.yip@carvana.com>
2023-11-01 16:32:24 -0700

    Encode with messageIndexes for confluent ksql support

--- src/wireDecoder.ts
+++ src/wireDecoder.ts
@@ -1,5 +1,6 @@
 export default (buffer: Buffer) => ({
   magicByte: buffer.slice(0, 1),
   registryId: buffer.slice(1, 5).readInt32BE(0),
-  payload: buffer.slice(5, buffer.length),
+  messageIndexes: buffer.slice(5, 6),
+  payload: buffer.slice(6, buffer.length),
 })
--- src/wireEncoder.ts
+++ src/wireEncoder.ts
@@ -2,9 +2,9 @@ const DEFAULT_OFFSET = 0
 
 export const MAGIC_BYTE = Buffer.alloc(1)
 
-export const encode = (registryId: number, payload: Buffer) => {
+export const encode = (registryId: number, payload: Buffer, messageIndexes: number[] = [0]) => {
   const registryIdBuffer = Buffer.alloc(4)
   registryIdBuffer.writeInt32BE(registryId, DEFAULT_OFFSET)
 
-  return Buffer.concat([MAGIC_BYTE, registryIdBuffer, payload])
+  return Buffer.concat([MAGIC_BYTE, registryIdBuffer, Buffer.from(messageIndexes), payload])
 }
