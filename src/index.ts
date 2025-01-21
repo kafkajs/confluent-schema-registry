3104a139c8f7e5e70ed7d8251ec54ef93762a60c
Adam Bergman <adam@fransvilhelm.com>
2025-01-21 11:36:17 +0100

    Update SchemaRegistry export to better support esm imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export { default as SchemaRegistry, DecodeOptions } from './SchemaRegistry'
+import { default as SchemaRegistry, DecodeOptions } from './SchemaRegistry'
+export { SchemaRegistry, DecodeOptions }
 export * from './utils'
 export { SchemaType } from './@types'
 export { COMPATIBILITY } from './constants'
